a292a1de7dc6f1e8d4b005d690656966e286a585
Andrew Reddin <areddin409@gmail.com>
2021-08-04 21:02:22 -0500

    Create Netflix Banner and pull data from TMDB database and populates background image, movie title and description

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAPJFXBx5WhaX2H4Q4db6m9R_mHWSKua0o',
+  authDomain: 'netflix-clone-8b776.firebaseapp.com',
+  projectId: 'netflix-clone-8b776',
+  storageBucket: 'netflix-clone-8b776.appspot.com',
+  messagingSenderId: '1039704444579',
+  appId: '1:1039704444579:web:dce07a10d895f465cb7916',
+};
