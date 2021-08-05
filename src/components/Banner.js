74f1e1ec59762c5fa4f1bd389fcba31c53c2afd1
Andrew Reddin <areddin409@gmail.com>
2021-08-05 09:56:25 -0500

    Created frontend UI

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -21,8 +21,6 @@ function Banner() {
     fetchData();
   }, []);
 
-  console.log(movie);
-
   function truncate(string, n) {
     return string?.length > n ? string.substring(0, n - 1) + '...' : string;
   }
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import requests from '../API/requests';
 import Banner from './Banner';
 import './HomeScreen.css';
 import Nav from './Nav';
+import Row from './Row';
 
 function HomeScreen() {
   return (
@@ -9,7 +11,19 @@ function HomeScreen() {
       <Nav />
 
       <Banner />
-      {/* Rows */}
+
+      <Row
+        title='NETFLIX ORIGINALS'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horrow Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
