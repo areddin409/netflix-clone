4ad6f76a4bb2ab10f1c39e7211f4633006ffa219
Andrew Reddin <areddin409@gmail.com>
2021-08-06 11:22:14 -0500

    Build Login Screen, Pagenation between profile, home, and login with redux. Profile screen still needs work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //Logged In
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        //Logged out
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
 
   return (
     <div className='app'>
@@ -14,6 +37,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path='/profile'>
+              <ProfileScreen />
+            </Route>
             <Route exact path='/'>
               <HomeScreen />
             </Route>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyAPJFXBx5WhaX2H4Q4db6m9R_mHWSKua0o',
   authDomain: 'netflix-clone-8b776.firebaseapp.com',
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: '1039704444579',
   appId: '1:1039704444579:web:dce07a10d895f465cb7916',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
